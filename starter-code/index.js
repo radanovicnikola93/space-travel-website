5cb460f64518419f10128c48cfcbcd1043e96adb
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-14 17:46:00 +0100

    added functionality to set the attribute true/false on the navigation

---
+++ starter-code/index.js
@@ -0,0 +1,18 @@
+// navigation
+const nav = document.querySelector('.primary-navigation');
+// button
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+// when someone click the hamburger button
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+
+    } 
+})
+
+
