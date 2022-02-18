4d728154c707cdc46a95812d9da220fc1c44f016
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-18 16:18:45 +0100

    added tabindex and starter files to tabs.js

---
+++ starter-code/tabs.js
@@ -0,0 +1,11 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', (e) => {
+    console.log(e.keyCode);
+    // storing key codes in separate variables
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    
+})
