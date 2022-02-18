8ab35118d9fd9d1bea4ce47f6eb7914a00827dd3
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-18 16:28:33 +0100

    added keycodes with interactiviy to change on the next or previous tab

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -1,11 +1,26 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+let tabFocus = 0;
+
+// ACCESSIBILITY
 tabList.addEventListener('keydown', (e) => {
-    console.log(e.keyCode);
     // storing key codes in separate variables
     const keydownLeft = 37;
     const keydownRight = 39;
 
-    
+    // change the tabindex of the current tab to -1
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
+
+    // if the right key is pushed, move to the next tab
+    if (e.keyCode === keydownRight) {
+        tabFocus++;
+    }
+
+    // if the left key is pushed, move to the previous tab
+    if (e.keyCode === keydownRight) {
+        tabFocus--;
+    }
 })
