d48ddaf338274327847051708c3982eff83a8cd6
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-19 10:33:30 +0100

    refactored code in one main if else stament

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -18,26 +18,26 @@ function changeTabFocus(e) {
     // change the tabindex of the current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
 
-    // if the right key is pushed, move to the next tab
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
+        // if the right key is pushed, move to the next tab
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
             tabFocus = 0;
+            }
         }
-    }
 
-    // if the left key is pushed, move to the previous tab
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
+        // if the left key is pushed, move to the previous tab
+        else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
             tabFocus = tabs.length - 1;
+            }
         }
-    }
 
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
+    }
 }
 
 // CHANGE TAB FOCUS
